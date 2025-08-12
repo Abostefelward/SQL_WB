5f215dd248826d8fcbf792a5742ab114e7935a20
abostefelward <abostefelward@gmail.com>
2025-08-12 10:19:44 +0200

    03_tables. upade.sql

--- 02_tables/03_tables.sql
+++ 02_tables/03_tables.sql
@@ -19,21 +19,21 @@ CREATE TABLE IF NOT EXISTS kunde
     k_id INT AUTO_INCREMENT PRIMARY KEY,
     k_name VARCHAR(45) NOT NULL DEFAULT 'TBA',
     k_alter TINYINT,
-    k_mail VARCHAR(100) UNIQUE,
-    k_password VARCHAR(255) NOT NULL
+    k_mail VARCHAR(100) UNIQUE
 );
 
 SHOW TABLES;
 
 DESCRIBE kunde;
 
-INSERT INTO kunde (k_id, k_name, k_alter, k_mail, k_password) VALUES (DEFAULT, 'Meier', 28, 'meier@web.de', '123pass');
-INSERT INTO kunde (k_id, k_name, k_alter, k_mail, k_password) VALUES (DEFAULT, 'Hussein', 32, 'hussein@web.de', '1234pass');
-INSERT INTO kunde (k_id, k_name, k_alter, k_mail, k_password) VALUES (DEFAULT, 'li', 26, 'li@web.de', '12345pass');
-INSERT INTO kunde (k_id, k_name, k_alter, k_mail, k_password) VALUES (DEFAULT, 'li', 40, 'lichan@gmail.com', '321pass');
-INSERT INTO kunde (k_id, k_name, k_alter, k_mail, k_password) VALUES (DEFAULT, 'li', 61, 'liyoun@gmail.com', '213pass');
+INSERT INTO kunde (k_id, k_name, k_alter, k_mail) VALUES (DEFAULT, 'Meier', 28, 'meier@web.de');
+INSERT INTO kunde (k_id, k_name, k_alter, k_mail) VALUES (DEFAULT, 'Hussein', 32, 'hussein@web.de');
+INSERT INTO kunde (k_id, k_name, k_alter, k_mail) VALUES (DEFAULT, 'li', 26, 'li@web.de');
+INSERT INTO kunde (k_id, k_name, k_alter, k_mail) VALUES (DEFAULT, 'li', 40, 'lichan@gmail.com');
+INSERT INTO kunde (k_id, k_name, k_alter, k_mail) VALUES (DEFAULT, 'li', 61, 'liyoun@gmail.com');
 
-INSERT INTO kunde(k_id, k_name, k_alter, k_mail, k_password) VALUES(DEFAULT,NULL,NULL,NULL,'412pass');
+INSERT INTO kunde(k_id, k_name, k_alter, k_mail) VALUES(DEFAULT,NULL,NULL,NULL);
+
 
 
 
